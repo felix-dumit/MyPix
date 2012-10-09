ec935527eb6675bbd80678bd7d4c77137f932b64
Felix Dumit <felix.dumit@gmail.com>
2012-10-09 12:14:02 +0800

    Loading initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CITS4230Ii::Application.config.secret_token = '6a65b002518c8aa8dd76fbb6de516d51ce1de8784017192f657df4dfe0418f5c1ab33019e0da2fccb18d3af49fd0d963d18af68e5089474431c86fa7be8f3472'
