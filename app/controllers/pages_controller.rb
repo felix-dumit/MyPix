9f6129258770976b79e2e6b04bb9b77ab70179af
Felix Dumit <felix.dumit@gmail.com>
2012-10-15 02:20:05 +0800

    Changed way to set pages title, now using provide function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,16 @@
 class PagesController < ApplicationController
   def home
-    @title = 'Home'
   end
 
   def mypage
-    @title = 'My Page'
   end
 
   def upload
-    @title = 'Upload'
   end
 
   def register
-    @title = 'Register'
   end
 
   def login
-    @title = 'Login'
   end
 end
