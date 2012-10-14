b564ac1fb20d862271b325a193d5f738cedd588f
Felix Dumit <felix.dumit@gmail.com>
2012-10-14 16:14:06 +0800

    Fixed relative path to images

--- app/assets/javascripts/reg_check.js
+++ app/assets/javascripts/reg_check.js
@@ -9,49 +9,49 @@ function checkPasswords() {
 }
 
 function checkDate(){
-	
+
 	var date = document.getElementById('reg_dob');
 	console.log('Date:'+date.value);
 	var m = date.value.search(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$|^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/);
 	if(m==-1)
 		date.setCustomValidity('Invalid date');
 	else date.setCustomValidity('');
-		
-		
-	
+
+
+
 }
 
 function createCaptcha(){
-//Generates the captcha function    
+//Generates the captcha function
 	var a = Math.ceil(Math.random() * 9)+'';
-	var b = Math.ceil(Math.random() * 9)+''; 
+	var b = Math.ceil(Math.random() * 9)+'';
 	var c = Math.ceil(Math.random() * 9)+'';
- 
+
 	var answer =+(a+b) + +(c);
 	console.log((''+'a:{0} b:{1} c:{2} ANS:{3}').format(a,b,c,answer));
 	document.getElementById("txtCaptcha").value = answer;
-	document.getElementById("txtCaptchaDiv").innerHTML = 'Please Answer: {0}{1} + {2} ='.format(a,b,c);	
+	document.getElementById("txtCaptchaDiv").innerHTML = 'Please Answer: {0}{1} + {2} ='.format(a,b,c);
 }
 
-	
-// Validate the Entered input aganist the generated security code function   
+
+// Validate the Entered input aganist the generated security code function
 function checkCaptcha(){
 	var val1 = document.getElementById('txtCaptcha');
 	var val2 = document.getElementById('txtInput');
 	if (val1.value == val2.value){
-		val2.setCustomValidity('');	
+		val2.setCustomValidity('');
 	}else{
-		val2.setCustomValidity('Invalid Captcha');	
+		val2.setCustomValidity('Invalid Captcha');
 	}
 }
 
 String.format = String.prototype.format = function() {
     var i=0;
     var string = (typeof(this) == 'function' && !(i++)) ? arguments[0] : this;
- 
+
     for (; i < arguments.length; i++)
         string = string.replace(/\{\d+?\}/, arguments[i]);
- 
+
     return string;
 }
 
@@ -59,9 +59,9 @@ String.format = String.prototype.format = function() {
 function checkLogin(){
 	var password = document.getElementById('pw');
 	var email = document.getElementById('email');
-	
+
 	//if(password.value=='' && email.value!='')alert('yooo');
-		
+
 }
 
 function addFileUpload(){
@@ -91,17 +91,17 @@ function addImage(img){
 	for( var i in mimg){
 		name=mimg[i];
 	}
-	name='img/user/'+name;
-	
+	name='assets/user/'+name;
+
 	var newImg = document.createElement('img');
 	newImg.setAttribute('src',name);
 	newImg.setAttribute('width','80px');
 	console.log(newImg);
 	img.parentNode.appendChild(newImg);
-	
+
 	imgUploads++;
-	
-	
+
+
 }
 
 function uploadImg(){
