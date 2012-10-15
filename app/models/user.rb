48b40f6bec4cd0793bdbdfeb56f4bd23165f0f0c
Felix Dumit <felix.dumit@gmail.com>
2012-10-15 20:49:16 +0800

    added password, and password verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,20 @@
 #
 # Table name: users
 #
-#  id            :integer          not null, primary key
-#  name          :string(255)
-#  email         :string(255)
-#  username      :string(255)
-#  date_of_birth :date
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  username        :string(255)
+#  date_of_birth   :date
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-    attr_accessible :name, :email, :username
+    attr_accessible :name, :email, :username, :password, :password_confirmation
+
+    has_secure_password
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     VALID_USERNAME_REGEX = /^[a-zA-Z0-9][a-zA-Z0-9_]*$/
@@ -21,5 +24,8 @@ class User < ActiveRecord::Base
     validates :username, presence: true, uniqueness: true,  format: { with: VALID_USERNAME_REGEX }
 
     before_save { |user| user.email = email.downcase }
+
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
 
 end
