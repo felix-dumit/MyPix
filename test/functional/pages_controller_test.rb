b5cc340e82092b4ec61f50cb78d795152522b0b2
Dylan Nangle <20518079@ecm-csse2511-l.uniwa.uwa.edu.au>
2012-10-20 22:57:19 +0800

    removed test from page controller

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -6,24 +6,4 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get mypage" do
-    get :mypage
-    assert_response :success
-  end
-
-  test "should get upload" do
-    get :upload
-    assert_response :success
-  end
-
-  test "should get register" do
-    get :register
-    assert_response :success
-  end
-
-  test "should get login" do
-    get :login
-    assert_response :success
-  end
-
 end
