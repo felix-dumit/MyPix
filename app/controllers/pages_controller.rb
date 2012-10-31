e01f6c52cd82cc17e27727fec8f402f3081e4093
Felix Dumit <felix.dumit@gmail.com>
2012-10-31 20:12:51 +0800

    fixed last images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 class PagesController < ApplicationController
   def home
-	@images = Image.find(:all, :order => "created_at", :limit => 9).reverse
+	@images = Image.last(9).reverse
   @urls = ['http://mypix.herokuapp.com', 'http://cits4230c.csse.uwa.edu.au/mypix']
-
   end
 
   def mypage
